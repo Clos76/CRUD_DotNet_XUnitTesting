3b50bc77992aecdb5c4c92957b3ea7eaf31c0944
Clos76 <robertoccg@proton.me>
2026-08-03 19:41:32 -0700

    search string created for url, GET Method

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -15,8 +15,18 @@ namespace CRUDExample.Controllers
         }
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string searchString)//model binding
         {
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                {nameof(PersonResponse.Email), "Email"},
+                {nameof(PersonResponse.Address), "Address"},
+                {nameof(PersonResponse.Gender), "Gender"},
+                {nameof(PersonResponse.CountryId), "Country ID" }
+
+            };
+
             List<PersonResponse> persons = _personService.GetAllPersons();
             return View(persons); //views/persons/index---- but we also supply the model value data
         }
